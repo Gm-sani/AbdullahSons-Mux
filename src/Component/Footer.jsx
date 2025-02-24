52ad3fb204b80e33c6f4da66fdb262a78fca5c4c
Gmsani <naeemmurtaza25@gmail.com>
2025-02-24 22:25:12 +0500

    24/feb(2nd)

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import footer from "./pics/footer_4.jpg"
+import footer from "./pics/footer4.jpg"
 import logo from "./pics/Logo.png"
 import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
 import XIcon from '@mui/icons-material/X';
